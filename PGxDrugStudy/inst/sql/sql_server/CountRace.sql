07c2d33c87f0ce8e419dd1fd7f6308d49104d904
Marc Suchard <msuchard@gmail.com>
2015-02-20 15:54:58 -0800

    Missed a SQL update

--- PGxDrugStudy/inst/sql/sql_server/CountRace.sql
+++ PGxDrugStudy/inst/sql/sql_server/CountRace.sql
@@ -1,6 +1,6 @@
-#  query-to-get-count-of-race-being-prescribed-any-drug-using-age-at-exposure.sql CountRace
+--  query-to-get-count-of-race-being-prescribed-any-drug-using-age-at-exposure.sql CountRace
 
-SELECT CONCEPT.concept_name as gender, COUNT(DISTINCT(PERSON.person_id)), 
+SELECT CONCEPT.concept_name as gender, COUNT(DISTINCT(PERSON.person_id)) 
 FROM DRUG_EXPOSURE,  PERSON, CONCEPT
 WHERE DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE >= DATE '2009-01-01'
 AND   DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE <= DATE '2012-12-31'
