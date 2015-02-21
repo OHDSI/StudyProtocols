242d0d1e04c9361c172d9165d65af6109b9ab1ce
Marc Suchard <msuchard@gmail.com>
2015-02-21 15:02:47 -0800

    All queries appear to work

--- PGxDrugStudy/R/Analysis.R
+++ PGxDrugStudy/R/Analysis.R
@@ -32,13 +32,9 @@
 execute <- function(dbms, user, password, server, 
                     port = NULL,
                     cdmSchema, resultsSchema, 
+										file = getDefaultStudyFileName(),
                     folder = getwd(),
-                    ...) {
-    
-    # Change working directory; MAY NOT BE NECESSARY DEPENDING ON STUDY
-    currentWd <- getwd()
-    setwd(folder)
-        
+                    ...) {        
     # Open DB connection
     connectionDetails <- DatabaseConnector::createConnectionDetails(dbms=dbms, 
                                                                     server=server, 
@@ -47,32 +43,102 @@ execute <- function(dbms, user, password, server,
                                                                     schema=cdmSchema,
                                                                     port = port)    
     conn <- DatabaseConnector::connect(connectionDetails)
-       
+        
     # Count gender
-    parameterizedSql <- SqlRender::readSql(system.file(paste("sql/","sql_server",sep=""), 
-                                                       "CountGender.sql", 
-                                                       package="PGxDrugStudy"))        
-    renderedSql <- SqlRender::renderSql(parameterizedSql)$sql    
-    translatedSql <- SqlRender::translateSql(renderedSql, 
-                                             sourceDialect = "sql server", 
-                                             targetDialect = dbms)$sql
-    genderResult <- DBI::dbGetQuery(conn, translatedSql)    
+    gender <- invokeSql("CountGender.sql", dbms, conn, "Executing gender count ...")    
     
-    result <- list()
+    # Count race
+    race <- invokeSql("CountRace.sql", dbms, conn, "Executing race count ...")
     
-    # Clean up and package result
-    DBI::dbDisconnect(conn)
-    setwd(currentWd)				
+    # Get frequencies
+    frequencies <- invokeSql("GetFrequencies.sql", dbms, conn, "Executing frequency count ...")
     
-    result$gender <- genderResult
-    # Return result																																    
-    class(result) <- "OhdsiStudy"    
-    return(result)
+    # Age by exposure
+    ageAtExposure <- invokeSql("AgeAtExposure.sql", dbms, conn, "Executing age by exposure count ...")
+    
+    # Age by exposure redefinition
+     ageAtExposureRedefinition <- invokeSql("AgeAtExposureRedefinition.sql", dbms, conn, "Executing age by exposure redefinition ...")
+     
+    # Count people
+    tmp <- invokeSql("CountPerson.sql", dbms, conn, text ="Executing person count ...", 
+    								 use.ffdf = TRUE) # Cache to disk in case table is large
+    person <- list()
+    person$count <- length(tmp[,1])
+    person$min <- min(tmp[,1])
+    person$median <- median(tmp[,1])
+    person$max <- max(tmp[,1])
+    rm(tmp) # discard potentially large file
+
+    # List of R objects to save
+    objectsToSave <- c(
+    	"gender",
+    	"race",
+    	"frequencies",
+    	"ageAtExposure",
+    	"ageAtExposureRedefinition",
+    	"person"
+    	)
+    
+    # Save results to disk
+    save(list = objectsToSave, file = file)
+            
+    # Clean up
+    DBI::dbDisconnect(conn)    
+
+    # Package and return result if return value is used	
+    result <- mget(objectsToSave)
+    class(result) <- "OhdsiStudy"
+    invisible(result)
+}
+
+#' @export
+saveOhsdiStudy <- function(list = stop("Must provide object list to save"),
+													 file = getDefaultStudyFileName(),
+													 compress = "xz",
+													 includeMetadata = TRUE) {
+	
+	
+	if (includeMetadata) {
+		metadata <- list()
+		
+		metadata$r.version <- R.Version()$version.string		
+		info <- Sys.info()
+		metadata$sysname <- info[["sysname"]]
+		metadata$login <- info[["login"]]
+		metadata$user <- info[["user"]]
+		metadata$nodename <- info[["nodename"]]
+		list <- c(list, "metadata")
+	}
+	
+	save(list = list,
+			 file = file,
+			 compress = compress)
+}
+
+#' @export
+invokeSql <- function(fileName, dbms, conn, text, use.ffdf = FALSE)  {
+	
+	parameterizedSql <- SqlRender::readSql(system.file(paste("sql/","sql_server",sep=""), 
+																									 fileName, 
+																									 package="PGxDrugStudy"))        
+	renderedSql <- SqlRender::renderSql(parameterizedSql)$sql    
+	translatedSql <- SqlRender::translateSql(renderedSql, 
+																				 sourceDialect = "sql server", 
+																				 targetDialect = dbms)$sql
+	writeLines(text)
+	if (use.ffdf) { 
+		return (DatabaseConnector::dbGetQuery.ffdf(conn, translatedSql))
+	} else {
+		return (DBI::dbGetQuery(conn, translatedSql))
+	}
 }
 
 
 #' @keywords internal
 getDestinationAddress <- function() { return("nobody@gmail.com") }
+
+#' @keywords internal
+getDefaultStudyFileName <- function() { return("PGxStudy.rda") }
 
 #' @title Email results
 #' 
