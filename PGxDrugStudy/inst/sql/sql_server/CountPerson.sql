0fee86527e50932a3cf9b7f438984192e154e88c
rkboyce <rdb20@pitt.edu>
2015-03-20 05:53:56 -0400

    addressing long identifiers

--- PGxDrugStudy/inst/sql/sql_server/CountPerson.sql
+++ PGxDrugStudy/inst/sql/sql_server/CountPerson.sql
@@ -1,7 +1,7 @@
 -- query-to-get-count-of-persons-being-prescribed-any-drug-using-age-at-exposure.sql
 
-WITH persons_and_their_age_at_first_prescription_in_time_window AS (
-		SELECT DISTINCT(DRUG_EXPOSURE.person_id), MIN((YEAR(DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE) - PERSON.year_of_birth)) AS person_age_at_first_prescription_in_time_window
+WITH persons_and_their_age AS (
+		SELECT DISTINCT(DRUG_EXPOSURE.person_id), MIN((YEAR(DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE) - PERSON.year_of_birth)) AS person_age_first_prescription
 		FROM DRUG_EXPOSURE,  PERSON
 		WHERE DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE >= DATE '2009-01-01'
 		AND   DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE <= DATE '2012-12-31'
@@ -10,7 +10,7 @@ WITH persons_and_their_age_at_first_prescription_in_time_window AS (
 		AND   (YEAR(DRUG_EXPOSURE.DRUG_EXPOSURE_START_DATE) - PERSON.year_of_birth < 14)
 		GROUP BY DRUG_EXPOSURE.person_id
 )
--- SELECT COUNT(person_id), min(person_age_at_first_prescription_in_time_window) as min_age, MAX(person_age_at_first_prescription_in_time_window) AS max_age, MIN(median_age_temp) AS median_age FROM 
---   (SELECT person_id, person_age_at_first_prescription_in_time_window, MEDIAN(person_age_at_first_prescription_in_time_window) OVER() AS median_age_temp
---     FROM persons_and_their_age_at_first_prescription_in_time_window)
-SELECT person_age_at_first_prescription_in_time_window FROM persons_and_their_age_at_first_prescription_in_time_window
+-- SELECT COUNT(person_id), min(person_age_at_first_prescription) as min_age, MAX(person_age_at_first_prescription) AS max_age, MIN(median_age_temp) AS median_age FROM 
+--   (SELECT person_id, person_age_at_first_prescription, MEDIAN(person_age_at_first_prescription) OVER() AS median_age_temp
+--     FROM persons_and_their_age)
+SELECT person_age_first_prescription FROM persons_and_their_age
