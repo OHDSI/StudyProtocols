9fdf692baa754a19f2c7049535a41b90be172b16
jreps <jreps@its.jnj.com>
2016-04-18 09:58:53 -0400

    Nice shiny interface

---
+++ CelecoxibPredictiveModelsUI/UI.R
@@ -0,0 +1,97 @@
+shiny::shinyUI(
+    shiny::fluidPage(
+        list(shiny::tags$head(shiny::HTML('<link rel="icon", href="www/predict2.jpg",
+                            type="image/png" />'))),
+        shiny::div(style="padding: 0px 0px; width: '100%'",
+                   shiny::titlePanel(title="", windowTitle="Celecoxib Predictive Models")
+        ),
+
+        shiny::navbarPage(
+            title=shiny::div("Celecoxib Predictive Models Study"),
+
+            shiny::tabPanel("Home",
+                            shiny::column(width=5,
+                                          shiny::imageOutput("image1")
+                     ),
+
+                     shiny::column(width=6,
+                                   shiny::p(shiny::h3("OHDSI Research Study- evaluating models across a network of databases"),
+                                            shiny::p("This study aims to investigate the robustness and transferablity of prediction models built for the same problem and using the same classifier (logistic regression with lasso regularisation) but across different datasets within the OHDSi community.")),
+                                   shiny::p(shiny::a(href="...", "More Information"))
+                     )
+
+            ),
+
+            #==============================================
+            # Check plp develop installed
+            #==============================================
+            # the data tab should have 3 conditional tabs - data loader/data extractor/data viewer
+            shiny::tabPanel("Install",
+                     shiny::mainPanel("Install study R packages:",
+                                      'Before running this package installer, please shut down any other R connections.',
+                                      DT::dataTableOutput("packageList"),
+
+                                      shiny::p('Click to install CelecoxibPredictionModels R Package and missing dependancies',
+                     shiny::actionButton('install', 'Install'))
+                         )
+
+
+
+
+                     ),
+
+            #========================================================
+            #  Study settings
+            #========================================================
+            shiny::tabPanel("Analysis",
+
+                            shiny::wellPanel(
+                                shiny::uiOutput("runAnalysis")
+
+                            )
+
+                      ),
+
+            shiny::tabPanel("Summary",
+                            shiny::uiOutput("selectData"),
+
+                            shiny::wellPanel(shiny::h4("Result Summary:"),
+                                DT::dataTableOutput("summary")
+                            )
+            ),
+
+            #========================================================
+            #  Send results
+            #========================================================
+            shiny::tabPanel("Explorer",
+                            shiny::uiOutput("visSel"),
+                            shiny::tabsetPanel(id ="analysisTabs",
+                                               shiny::tabPanel(title = "Performance", value="panel_permform",
+                                                               shiny::h4("Performance Metrics"),
+                                                               DT::dataTableOutput("performance")),
+                                               shiny::tabPanel(title = "Variables", value="panel_varimp",
+                                                               shiny::h4("Variable Importance"),
+                                                               DT::dataTableOutput("varImp")),
+                                               shiny::tabPanel(title = "Attrition", value="panel_attrition",
+                                                               shiny::h4("Attrition"),
+                                                               DT::dataTableOutput("attrition")),
+                                               shiny:: tabPanel(title = "ROC", value="panel_roc",
+                                                                shiny::h4("ROC Plot"),
+                                                                shiny::plotOutput("rocPlot")),
+                                               shiny::tabPanel(title = "Box Plot", value="panel_box",
+                                                               shiny::h4("Box Plot"),
+                                                               shiny::plotOutput("boxPlot")),
+                                               shiny::tabPanel(title = "Calibration", value="panel_cal",
+                                                               shiny::h4("Calibration Plot"),
+                                                               shiny::plotOutput("calPlot")),
+                                               shiny::tabPanel(title = "Preference", value="panel_pref",
+                                                               shiny::h4("Preference Scores"),
+                                                               shiny::plotOutput("prefPlot"))
+                            )
+
+            )
+
+
+        )
+        )
+    )
