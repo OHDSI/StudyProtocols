55d0748d28d7aa0edb0659be12e10bd046a06ed4
Yuxi Tian <yuxitian@gmail.com>
2017-11-08 15:08:46 +0100

    figuring out figure sizes; hacked figure labels

--- AlendronateVsRaloxifene/extras/labelFigures.R
+++ AlendronateVsRaloxifene/extras/labelFigures.R
@@ -7,14 +7,21 @@ saveTable(obj, filename = "outcomeCountHipFracture")
 # Figure 1
 saveFigure("extras/figures/unnamed-chunk-2-1.pdf", "cohortYear",
            caption = "Year of study entry, stratified by drug exposure and data source. Note patient counts are on the log-scale",
-           1, "4.00in", "7.53in")
+           1, "4.00in", "7.53in", newWidth = "3in")
 
 # Figure 2
 saveFigure("extras/figures/unnamed-chunk-3-1.pdf", "cohortAge",
            caption = "Age at study entry, stratified by drug exposure and data source. Note patient counts are on the log-scale",
-           2, "4.04in", "7.60in")
+           2, "4.04in", "7.60in", newWidth = "3in")
 
 # Figure 3
 saveFigure("extras/figures/unnamed-chunk-40-2.pdf", "forestPlots",
            caption = "Main analysis hazard ratios for A) hip fracture, B) vertebral fracture, and C) atypical femoral fracture. More precise estimates have greater opacity",
-           3, "6.90in", "7.99in")
+           3, "6.90in", "7.99in", newWidth = "5in")
+
+# Figure 4
+saveFigure("KM_Optum.png", "KM_Optum_label", caption = "Kaplan-Meier plot for hip fracture outcome in Optum CEDM data source",
+           4, width = "309.3pt", height = "184.5pt")
+
+# pixel to pt factor: 72.27/600
+
